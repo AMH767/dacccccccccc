feae48512e811c6dfa6becaa1f1d3817df5a2876
HMA <125194472+AMH767@users.noreply.github.com>
2025-10-27 15:29:18 +0300

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -35,6 +35,7 @@ class CustomNavbar extends HTMLElement {
           list-style: none;
           margin: 0;
           padding: 0;
+          margin-right: 70px;
         }
         a {
           color: #6b513a;
